69b8ab6cd42984824baa8f05c5ab37cc9352a1af
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-07-10 21:20:54 -0300

    terminando o style

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -136,39 +136,3 @@ const montandoCards = (objprodutos) => {
     })
 
 }
-
-todosSecoes()
-const todosSecoes = (objprodutos) => {
-  section_cards.innerHTML = '';
-
-
-    objprodutos.forEach((elem, i) => {
-        const navSecoes = document.createElement('nav');
-        navSecoes.setAttribute('class');
-
-        const imgProduto = document.createElement('img');
-        imgProduto.setAttribute('src', elem.caminho_da_imagem);
-        imgProduto.setAttribute('alt', elem.descricao_produto);
-        imgProduto.setAttribute('class', 'img_card');
-
-        const h2Titulo = document.createElement('h2');
-        h2Titulo.innerHTML = elem.descricao_produto;
-
-        const h3Valor = document.createElement('h3');
-        h3Valor.setAttribute('class', 'valor_card');
-        // Correção aplicada: uso de crases (``) para que a interpolação ${} funcione corretamente
-        h3Valor.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.', ',')}`;
-
-        const btnCard = document.createElement('button');
-        btnCard.setAttribute('class', 'btn_card');
-        btnCard.innerHTML = 'Adicionar';
-
-        navSecoes.appendChild(imgProduto);
-        navSecoes.appendChild(h2Titulo);
-        navSecoes.appendChild(h3Valor);
-        navSecoes.appendChild(btnCard);
-
-        section_cards.appendChild(divCard);
-    })
-
-}
