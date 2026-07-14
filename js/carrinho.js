6ca1caa3f7d112d7548f233fd528634f9e423918
vivian suenny <vivian.cruz@aluno.senai.br>
2026-07-13 21:38:24 -0300

    terminando o projeto

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,14 +1,36 @@
 //Criando o Array de Itens do Carrinho
-const ItensCarrinho = []
+const ItensCarrinho = JSON.parse(localStorage.getItem('itensSessao')) || []
 
 //Função para adicionar o item no Array
 const addItem = (objItem)=> {
     ItensCarrinho.push(objItem)
+
+    localStorage.setItem('itensSessao' , ItensCarrinho)
 }
 
 //Listar Itens no Carrinho
 const listItens = () => {
+    const ItensSelecionados = JSON.stringify(localStorage.getItem('intensSessao'))
+
     return ItensCarrinho
 }
 
+//Montando a tela do Carrinho
+const montaTelaCarrinho = () => {
+    //Pegando Elementos do Dom
+    const sectionItensCarrinho = document.querySelector('#itens-carrinho')
+
+    listItens().forEach((elem, i) => {
+      const sectionItem = document.createElement('section')
+      sectionItem.setAttribute('class','item')
+      sectionItem.innerHTML = `<img src='${elem.caminho_da_imagem}' alt=${elem.descricao_produto}/>
+      <p class='descricao'>${elem.descricao_produto}</p> 
+        <p class='vlr-unitario'>${elem.valor_unitario}</p> 
+        <input type="number" name='quant${i}' id='quant${i}' class="input-item" value="1"> 
+        <p class='tot-item'>${elem.valor_unitario * 1}</p>
+        <img src="../imagens/icones/remover.png" alt="" class="img-remover">`
+    
+        sectionItensCarrinho.appendChild(sectionItem)
+    });
+}
 export{addItem}
