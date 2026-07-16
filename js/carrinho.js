40b7e5df8756fd10d1573e9d7cd698fe2c3fa851
vivian suenny <vivian.cruz@aluno.senai.br>
2026-07-15 21:39:50 -0300

    terminando carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -7,15 +7,34 @@ const ItensCarrinho = JSON.parse(localStorage.getItem('itensSessao')) || []
 
 //const ItensCarrinho = JSON.parse(sessionStorage.getItem('itensSessao')) || []
 
+//Criando Arrom Item
+const fobjItem = (objProduto) => {
+    const item= {
+    id_produto: objProduto.id_produto,
+    descricao_produto: objProduto.descricao_produto,
+    caminho_da_imagem: objProduto.carrinho_da_imagem,
+    valor_unitario: objProduto.valor_unitario,
+    quantidade: 1
+    }
+
+    return item
+}
+
+//Pegando o indice do Array
+console.log("índice do array ->>>",ItensCarrinho.finIndex(elem => elem.id_produto == 12))
+
 //Função para adicionar o item no Array
 const addItem = (objItem) => {
-    ItensCarrinho.push(objItem)
+    ItensCarrinho.push(fobjItem (objItem))
 
     localStorage.setItem('itensSessao', JSON.stringify(ItensCarrinho))
+
+    //sessionStorage.setItem(`itensSessao`, JSON.stringify(itensCarrinho))
 }
 
 //Listar Itens no Carrinho
 const listItens = () => {
+    
     const ItensSelecionados = JSON.parse(localStorage.getItem('itensSessao')) || []
 
     return ItensSelecionados
